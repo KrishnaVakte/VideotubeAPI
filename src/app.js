0d0b5a1fd627709e9d70a2b3c4db4e581363cb21
Krishna Vakte <kvakte02@gmail.com>
2024-06-09 10:30:14 +0530

    add authorization allow

--- src/app.js
+++ src/app.js
@@ -15,7 +15,8 @@ app.use(cookieParser())
 
 app.use(cors({
     origin: [process.env.CORS_ORIGIN, 'http://localhost:5173'],
-    credentials : true
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 app.use(express.json({ limit: "20kb" }))
