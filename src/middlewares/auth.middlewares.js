0a5354845648240cb0e123362e2c6bf6f65af43d
Krishna Vakte <kvakte02@gmail.com>
2024-06-09 09:25:29 +0530

    test

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -6,7 +6,9 @@ import { User } from "../models/user.models.js";
 
 const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
+        console.log(req)
         const token = req.cookies?.accessToken || req.body?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        console.log(token)
         if (!token) {
             throw new ApiError(400,"Unauthorized request")
         }
